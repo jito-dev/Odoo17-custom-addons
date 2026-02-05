bcc52781ee43108c75e29e0cd0b3637610654331
Zakhar Bozhok <zakhar.bozhok@jito.dev>
2026-02-05 08:00:58 +0000

    Updated UI on RCE

--- tm_rate_card/__manifest__.py
+++ tm_rate_card/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Rate Card Management (Pricing Authority)',
     'icon': 'tm_rate_card/static/description/icon.png',
-    'version': '1.14.3',
+    'version': '1.14.4',
     'category': 'Services/Project',
     'summary': 'Time & Materials Rate Card - Single Source of Truth for T&M Pricing',
     'description': """
