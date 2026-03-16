e87642200672cdac2330cdb607b1edfaefa19e4a
Zakhar Bozhok <zakhar.bozhok@jito.dev>
2026-03-16 19:48:33 +0000

    Allow for Income Reviewer to Sync

--- legacy_accounting_helper/__manifest__.py
+++ legacy_accounting_helper/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Revolut Business API Integration',
-    'version': '17.0.1.36.0',
+    'version': '17.0.1.37.0',
     'category': 'Accounting',
     'summary': 'Revolut Business API OAuth2 setup helper',
     'description': """
