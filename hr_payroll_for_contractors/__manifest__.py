f7d38d2145813867c1ff178dac63a635a312df0a
Zakhar Bozhok <zakhar.bozhok@jito.dev>
2026-04-02 15:32:31 +0000

    Added description view + buttons for other roles to recompute tracks

--- hr_payroll_for_contractors/__manifest__.py
+++ hr_payroll_for_contractors/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Payroll for Contractors',
-    'version': '1.5.4',
+    'version': '1.5.7',
     'category': 'Human Resources/Payroll',
     'summary': 'Manage contractor payroll based on timesheets',
     'description': """
