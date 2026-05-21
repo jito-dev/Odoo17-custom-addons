66e3a8aa5a7209eeeab5453f97d822c29448740e
Olena Maksymiv <olena.maksymiv@jito.dev>
2026-05-21 15:13:49 +0000

    Revert "fix: fix stages logic"
    This reverts commit d12dd493d5f54bd99f5933954e221f00a9befce7.

--- hr_recruitment_test_task/__manifest__.py
+++ hr_recruitment_test_task/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Recruitment Test Task',
-    'version': '17.0.1.0.3',
+    'version': '17.0.1.0.4',
     'category': 'Human Resources/Recruitment',
     'summary': 'Manage technical test task submissions for candidates',
     'description': """
@@ -12,6 +12,8 @@
     """,
     'author': "alextranduil",
     'website': "https://jito.dev",
+    'category': 'Human Resources/Recruitment',
+    'version': '17.0.1.0.1',
     'depends': ['hr_recruitment', 'website', 'mail'],
     'data': [
         'security/ir.model.access.csv',
@@ -21,7 +23,6 @@
         'views/hr_job_views.xml',
         'views/website_templates.xml',
     ],
-    'pre_init_hook': 'pre_init_hook',
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
